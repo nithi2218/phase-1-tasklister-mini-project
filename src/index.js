c65a331a1106e877d8b905c51f11f4f80fd7b95c
Nithyadevi <nithi2218@gmail.com>
2022-03-26 11:01:52 +1100

    Not completed yet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ document.addEventListener("DOMContentLoaded", () => {
+    const submitBtn = document.querySelector('input[type=submit]');
+    const formCreate = document.getElementById('new-task-description')
+    const createList = document.getElementById("tasks");
+   const fromList = document.getElementById('create-task-form') 
+
+    const addListItem = (elementValue) => `<li class="sublisting">${elementValue}</li>`;
+ 
+
+    let formSubmitHandler = (event) => {
+        event.preventDefault();
+        let elementValue = formCreate.value;
+        createList.innerHTML += addListItem(elementValue)
+           
+    }    
+
+    fromList.addEventListener('submit', formSubmitHandler);
+    
+    
+    let dlBtn = document.createElement("button");
+    dlBtn.innerHTML = "x";
+function deleteList() {
+    dlBtn.addEventListener(click, e => {
+        childNode.remove();
+    })
+    
 });
