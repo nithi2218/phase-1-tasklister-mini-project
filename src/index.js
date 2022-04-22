0c8c22c7fa71ededc09a88a87934e02a027cbfb1
Nithyadevi <nithi2218@gmail.com>
2022-04-22 21:22:55 +1000

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,64 @@
- document.addEventListener("DOMContentLoaded", () => {
-    const submitBtn = document.querySelector('input[type=submit]');
-    const formCreate = document.getElementById('new-task-description')
-    const createList = document.getElementById("tasks");
-   const fromList = document.getElementById('create-task-form') 
+document.addEventListener('DOMContentLoaded', () => {
+    class Task {
+      constructor(desc) {
+        this.description = desc;
+      }
+  
+      render() {
+        return this.description
+      }
+  
+    }
+  
+    class TaskList {
+      // this class should have all the tasks
+      constructor(){
+        this.tasks = []
+      }  
+  
+      // create tasks
+      createTask(description) {
+        const tempTask = new Task(description)
+        this.tasks.push(tempTask)
+      }
+  
+      // delete
+      function removeToDoItem() {
+        newLi.toDoList.removeChild(newLi);
+        renderTheUI(toDos);
+      }
 
-    const addListItem = (elementValue) => `<li class="sublisting">${elementValue}</li>`;
- 
-
-    let formSubmitHandler = (event) => {
-        event.preventDefault();
-        let elementValue = formCreate.value;
-        createList.innerHTML += addListItem(elementValue)
-           
-    }    
-
-    fromList.addEventListener('submit', formSubmitHandler);
+      // render/createHTML to be shown on screen/user
+  
+      renderTasks() {
+        // loop through the tasks array and prepare HTML
+        let htmlString = ''
+        for(let task of this.tasks){
+          htmlString += `<li>${task.render()}</li>`
+        }
+  
+        return htmlString
+      }
+  
+    }
+  
+    let tl = new TaskList()
+  
+  
     
-    
-    let dlBtn = document.createElement("button");
-    dlBtn.innerHTML = "x";
-function deleteList() {
-    dlBtn.addEventListener(click, e => {
-        childNode.remove();
+  
+    taskForm = document.getElementById('create-task-form')
+    descInput = document.getElementById('new-task-description')
+    taskUl = document.getElementById('tasks')
+  
+    taskForm.addEventListener('submit',(event) => {
+      event.preventDefault();
+      // call create new task
+      tl.createTask(descInput.value)
+      taskForm.reset()
+      taskUl.innerHTML = tl.renderTasks()
     })
-    
-});
+  });
+  
+  
+  
